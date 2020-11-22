e2f83e7b06961fd8c7ea1804d8c13e15d7326381
kurobane.kengo <kurobanekengo@gmail.com>
2020-11-22 18:44:14 +0900

    server: 起動時にホストとIPをログ出力

--- source/start.ts
+++ source/start.ts
@@ -4,4 +4,6 @@ import "@api/password/password";
 
 const config = getConfig();
 server.use(`/${config.api.prefixPath}`, router);
-server.listen(config.api.port, config.api.host, () =>{});
+server.listen(config.api.port, config.api.host, () =>{
+  console.log(`listen on ${config.api.host}:${config.api.port}`)    
+});
